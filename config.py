6e9c79ff023fdbf8fc4559850ff9fb57fa99c1a0
Tu-Tam <letutam36@gmail.com>
2026-09-02 20:55:24 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,16 @@
-# Cấu hình hệ thống XSMB
-DATABASE_PATH = "xsmb_data.db"
+import os
+
+# Cấu hình Token & Server
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "")
+PORT = int(os.environ.get("PORT", 10000))
+
+# Cấu hình danh mục giải Vietlott
+GAME_CONFIG = {
+    "655": {"name": "Power 6/55", "max_num": 55, "pick": 6, "type": "standard"},
+    "645": {"name": "Mega 6/45", "max_num": 45, "pick": 6, "type": "standard"},
+    "3d": {"name": "Max 3D", "length": 3, "type": "digit"},
+    "keno": {"name": "Keno", "max_num": 80, "pick": 20, "type": "standard"},
+}
+
+# Đường dẫn Database
+DB_FILE = "vietlott.db"
